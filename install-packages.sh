46f2c9e021e00f821582a8bc57987f459578f8a9
root <root@ip-172-31-30-177.eu-central-1.compute.internal>
2020-01-11 06:50:16 +0000

    OS check option added

--- install-packages.sh
+++ install-packages.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+ok() { echo -e '\e[32m'$1'\e[m'; } # Green
+
+if [[ -e /etc/redhat-release ]]; then
+    RELEASE_RPM=$(rpm -qf /etc/centos-release)
+    RELEASE=$(rpm -q --qf '%{VERSION}' ${RELEASE_RPM})
+    if [ ${RELEASE} != "7" ]; then
+        echo "Not CentOS release 7."
+        exit 1
+    fi
+else
+    echo "Not CentOS system."
+    exit 1
+fi
+
+ok "Installing packages..."
+
 sudo yum -y update
 sudo yum install -y epel-release
 sudo yum install -y nginx curl wget vim redis varnish haproxy unzip expect libxml2
 sudo yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 sudo yum-config-manager --enable remi-php71
 sudo yum install -y php php-common php php-mysql php-fpm php-intl php-mbstring php-soap php-zip php-bcmath
+
+if [ $? != 0 ]; then exit 1; fi
